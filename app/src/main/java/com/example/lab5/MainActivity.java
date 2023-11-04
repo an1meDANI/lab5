9c85783171c23a040f56fa869e2e7698f74bbbe0
Dan Miller <anime.dani@mail.ru>
2023-11-04 21:33:41 +0500

    Initiаl

---
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -0,0 +1,140 @@
+package com.example.lab5;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private SupportFragment supportFragment = new SupportFragment();
+    private TestingFragment testingFragment = new TestingFragment();
+    private NotificationFragment notificationFragment = new NotificationFragment();
+
+
+    private static final String CHANEL_ID = "ch1";
+    private NotificationManagerCompat notificationManagerCompat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.support_btn:
+
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fragment1, supportFragment)
+                                .commit();
+                        return true;
+
+                    case R.id.testing_btn:
+
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fragment1, testingFragment)
+                                .commit();
+                        return true;
+                    case R.id.notification_btn:
+
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fragment1, notificationFragment)
+                                .commit();
+                        return true;
+
+                }
+                return false;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.support_btn);
+    }
+
+    public void onclick_support(View view) {
+        EditText fio = findViewById(R.id.editTextTextPersonName);
+        fio.getText().clear();
+        EditText email = findViewById(R.id.editTextTextPersonName2);
+        email.getText().clear();
+        Toast toast = Toast.makeText(getApplicationContext(), "Данные отправлены успешно!", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void onclick_test(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        String[] items = {"Андрей", "Дмитрий", "Алексей", "Даниил"};
+        List<String> fruit_list = Arrays.asList(items);
+        boolean[] checkItems = {false, false, false, false};
+        builder.setMultiChoiceItems(items, checkItems, (dialog, which, isChecked) -> {
+            checkItems[which] = isChecked;
+            String currentItem = fruit_list.get(which);
+            if (currentItem == "Алексей") {
+                Toast toast = Toast.makeText(getApplicationContext(), "Верно!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+            else {
+                Toast toast = Toast.makeText(getApplicationContext(), "Попробуй еще раз!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+        builder.show();
+        builder.create();
+    }
+
+    public void onclick_notification(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANEL_ID,
+                    "ch2",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManagerCompat = notificationManagerCompat.from(this);
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(getApplicationContext(), CHANEL_ID)
+                        .setSmallIcon(R.drawable.test_icon)
+                        .setContentTitle("Золотарев Даниил Андреевич")
+                        .setContentText("Т-403901-НТ")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        notificationManagerCompat.notify(1, builder.build());
+    }
+}
